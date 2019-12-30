7f1ede94a2770dc49cb1dd6e2f39ec8e23db16b4
BBlaine293 <55700742+BBlaine293@users.noreply.github.com>
2019-12-30 00:57:26 +0000

    Menus added
    Added main menu
    pause menu
    etc

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public string startlevel;
+    public string options;
+
+    
+    public void StartGame()
+    {
+        Application.LoadLevel(startlevel);
+    }
+
+    public void Options()
+    {
+        Application.LoadLevel(options);
+    }
+     
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+   
+    
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public string MainMenu;
+    public bool isPaused;
+    public GameObject pausemenu;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPaused)
+        {
+            pausemenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            pausemenu.SetActive(false);
+            Time.timeScale = 1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        
+
+
+
+    }
+    
+    public void Resume()
+    {
+        isPaused = false;
+    }
+
+    public void Quit()
+    {
+        Application.LoadLevel(MainMenu);
+    }
+
+
+}
